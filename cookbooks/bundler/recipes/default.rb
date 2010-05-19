4d1ebfa39c12fd442ba7f23ad0fa5306d7d2b8f6
Seamus Abshere <seamus@abshere.net>
2010-05-19 19:34:00 -0400

    install the bundle as the deploy user

--- cookbooks/bundler/recipes/default.rb
+++ cookbooks/bundler/recipes/default.rb
@@ -7,7 +7,7 @@ if %w{app app_master solo}.include?(node[:instance_role])
 
   node[:applications].each do |app_name, data|
     execute "run bundle install" do
-      user 'root'
+      user node[:owner_name]
       cwd "/data/#{app_name}/current"
       command 'bundle install'
     end
